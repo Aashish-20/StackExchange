72c4092b9c33dca6b623419369aa324263cb7699
AASHISH <aashishkumar10122000@gmail.com>
2021-07-19 21:41:18 +0530

    added splash activity

--- app/src/main/java/com/example/qa/MainActivity.kt
+++ app/src/main/java/com/example/qa/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.qa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        splash()
+    }
+
+    private fun splash(){
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, SearchActivity::class.java))
+            },
+            2000)
     }
 }
