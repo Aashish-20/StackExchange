2727917ad85f85be9682a58b98b8052305c2da12
AASHISH <aashishkumar10122000@gmail.com>
2021-07-19 21:01:06 +0530

    project setup1

--- app/src/main/java/com/example/qa/MainActivity.kt
+++ app/src/main/java/com/example/qa/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    var inc:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
